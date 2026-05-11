8d868e9bd2c869834e7e21257b95c569d5eda115
Xiaoli <xwei@amt.rwth-aachen.de>
2026-05-11 15:26:28 +0200

    Remove unused ROS1-style IMU reset declaration

--- third_party/FAST_LIO/src/IMU_Processing.hpp
+++ third_party/FAST_LIO/src/IMU_Processing.hpp
@@ -39,7 +39,6 @@ class ImuProcess
   ~ImuProcess();
   
   void Reset();
-  void Reset(double start_timestamp, const sensor_msgs::msg::Imu::ConstSharedPtr &lastimu);
   void set_extrinsic(const V3D &transl, const M3D &rot);
   void set_extrinsic(const V3D &transl);
   void set_extrinsic(const MD(4,4) &T);
